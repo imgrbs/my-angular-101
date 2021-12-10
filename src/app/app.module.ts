4f577f8e64f47950b31589a2473c306568770fb6
Imagine Rabbits <keerati.jear@gmail.com>
2021-12-10 13:02:49 +0700

    Add material design and use them to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { ButtonComponent } from './button/button.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     ButtonComponent,
-    PostListComponent
+    PostListComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
